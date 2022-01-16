fd3adfbfd45e455e9eb4b2ea88b17f50cf073743
keitsune <tsune.kei.32821@gmail.com>
2022-01-16 08:26:03 +0000

    [Add] topページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+
+  root 'homes#top'
+
+  scope module: :public do
+
+  end
+
+
   devise_for :admins
   devise_for :kids
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+
+  end
+
 end
